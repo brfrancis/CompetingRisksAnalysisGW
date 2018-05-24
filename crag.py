adc75dd3fb4c66ead42bc99a2fd7db6f2245a595
brfrancis <brfrancis@users.noreply.github.com>
2018-05-24 14:10:34 +0100

    update to ensure compatibility across Linux and Windows

--- crag.py
+++ crag.py
@@ -29,7 +29,7 @@ import itertools
 import time
 import warnings
 #import progressbar
-#from multiprocessing.dummy import Pool as ThreadPool 
+#from multiprocessing.dummy import Pool as ThreadPool
 
 def HWE_test_chi(obs_AA, obs_Aa, obs_aa, miss):
 	obs_t = (obs_Aa + obs_AA + obs_aa - miss)
@@ -44,10 +44,10 @@ def HWE_test_chi(obs_AA, obs_Aa, obs_aa, miss):
 
 def maf_calc(gp0n,gp1n,gp2n,a_l,miss):
 	if (gp0n>gp2n):
-		maf=((2*gp2n+gp1n)/(2*(a_l-miss))) 
+		maf=((2*gp2n+gp1n)/(2*(a_l-miss)))
 	else:
 		maf=((2*gp0n+gp1n)/(2*(a_l-miss)))
-	return maf	
+	return maf
 
 def info_calc(gp0,gp1,gp2,a_l,miss):
 	N = a_l - miss
@@ -73,13 +73,13 @@ def HWE_test(obs_hom2, obs_het, obs_hom1):
 
 	obs_homc = int(obs_hom2) if obs_hom1 < obs_hom2 else int(obs_hom1)
 	obs_homr = int(obs_hom1) if obs_hom1 < obs_hom2 else int(obs_hom2)
-	obs_hets = int(obs_het)	
+	obs_hets = int(obs_het)
 
 	rare_copies = 2 * obs_homr + obs_hets
 	genotypes   = obs_hets + obs_homc + obs_homr
 
 	het_probs = [0.0] * (rare_copies + 1)
-	
+
 	#start at midpoint
 	mid = int(rare_copies * (2 * genotypes - rare_copies) / (2 * genotypes))
 
@@ -90,14 +90,14 @@ def HWE_test(obs_hom2, obs_het, obs_hom1):
 	curr_hets = int(mid)
 	curr_homr = int((rare_copies - mid) / 2)
 	curr_homc = int(genotypes - curr_hets - curr_homr)
-	
+
 	het_probs[mid] = 1.0
 	sum = float(het_probs[mid])
-	
+
 	for curr_hets in range(mid, 0, -2):
 
 		het_probs[curr_hets - 2] = het_probs[curr_hets] * curr_hets * (curr_hets - 1.0) / (4.0 * (curr_homr + 1.0) * (curr_homc + 1.0))
-		
+
 		sum += het_probs[curr_hets - 2]
 		# 2 fewer heterozygotes for next iteration -> add one rare, one common homozygote
 		curr_homr += 1
@@ -106,11 +106,11 @@ def HWE_test(obs_hom2, obs_het, obs_hom1):
 	curr_hets = mid
 	curr_homr = (rare_copies - mid) / 2
 	curr_homc = genotypes - curr_hets - curr_homr 
-	
+
 	for curr_hets in range(mid, rare_copies - 1, 2):
 
 		het_probs[curr_hets + 2] = het_probs[curr_hets] * 4.0 * curr_homr * curr_homc / ((curr_hets + 2.0) * (curr_hets + 1.0))
-		
+
 		sum += het_probs[curr_hets + 2]
 		# 2 more heterozygotes for next iteration -> add one rare, one common homozygote
 		curr_homr -= 1
@@ -118,7 +118,7 @@ def HWE_test(obs_hom2, obs_het, obs_hom1):
 
 	for i in range(0, rare_copies + 1):
 		het_probs[i] /= sum
-	
+
 	#alternate p-value calculation for p_hi/p_lo
 	p_hi = float(het_probs[obs_hets])
 	for i in range(obs_hets, rare_copies+1):
@@ -129,7 +129,7 @@ def HWE_test(obs_hom2, obs_het, obs_hom1):
 		p_lo += het_probs[i]
 
 	p_hi_lo = 2.0 * p_hi if p_hi < p_lo else 2.0 * p_lo
-	
+
 	p_hwe = 0.0
 	#  p-value calculation for p_hwe
 	for i in range(0, rare_copies + 1):
@@ -139,7 +139,7 @@ def HWE_test(obs_hom2, obs_het, obs_hom1):
 
 	p_hwe = 1.0 if p_hwe > 1.0 else p_hwe
 
-	return p_hwe	
+	return p_hwe
 
 
 
@@ -147,135 +147,146 @@ def do_work(in_queue, out_list, sub):
 	while True:
 		item = in_queue.get()
 		line_no, line = item
-	# exit signal 
+	# exit signal
 		if line == None:
 			return
-		
+
 		if args.r == 1:
 			# Using R work
 			result = crgwas(line, line_no )
 		if args.r == 0:
 			# Using lifelines work for cause-specific
-			result = pycrgwas(line, line_no)
-		
+			gp, info, maf, hwe, gl = QC(line, line_no)
+			df3_1 = pycrgwas(gp, sub)
+			result = output(df3_1, info, maf, hwe, gl)
 		# output
 		out_list.append(result)
-	
+
+def gwasprogress( ):
+	sys.stdout.write('\r')
+	progress = "#" + str(i-args.thread) + " SNPS ANALYSED @ {0:0.01f} SECS PER SNP #".format((time.time()-start)/i)
+	#sys.stdout.write(progress.center(os.get_terminal_size().columns))
+	sys.stdout.write(progress)
+	sys.stdout.flush()
+	work.put(num_and_line)
+
 def crgwas(line, line_no ):
-		gl=line.split(' ');	
-		gp0j=gl[5::3]
-		gp1j=gl[6::3]
-		gp2j=gl[7::3]
-		
-		gp0=[float(i) for i in gp0j]
-		gp1=[float(i) for i in gp1j]
-		gp2=[float(i) for i in gp2j]
-		
-		gp = [];a_l=len(gp1)
-		miss=0
-		for i in range(a_l):
-			if (gp0[i]+gp1[i]+gp2[i])>0:
-				gp.append(gp1[i]+(2*gp2[i]))
-			else:
-				gp.append(np.nan)
-				miss+=1
-		gp0n=np.nansum(gp0)
-		gp1n=np.nansum(gp1)
-		gp2n=np.nansum(gp2)
-		
-		#QC measures
-		
-		info=info_calc(gp0,gp1,gp2,a_l,miss)
-		maf=maf_calc(gp0n,gp1n,gp2n,a_l,miss)
-		hwe=HWE_test(gp0n,gp1n,gp2n)
+	gl=line.split(' ')
+	gp0j=gl[5::3]
+	gp1j=gl[6::3]
+	gp2j=gl[7::3]
+	gp0=[float(i) for i in gp0j]
+	gp1=[float(i) for i in gp1j]
+	gp2=[float(i) for i in gp2j]
 
-		#Analysis
-		
-		sub['gz']=gp
-		sub[[args.t_pheno, args.et_pheno]] = sub[[args.t_pheno, args.et_pheno]].astype(int)
-		robjects.globalenv['sub'] = sub
-		while True:
-			try:
-				test=cmprsk.crr(ftime=sub[args.t_pheno],fstatus=sub[args.et_pheno],cov1=sub.iloc[:,2:],failcode=args.obs,cencode=0)
-				res=base.summary(test).rx2('coef')
-				#resconv=base.summary(test).rx2('conv') STILL NEED TO CODE CONV
-				df3 = pd.DataFrame({'beta':[res.rx('gz',1)],'se':[res.rx('gz',3)],'p':[res.rx('gz',5)],'conv':[1]})
-				df3['beta'] = df3['beta'].str[0]
-				df3['se'] = df3['se'].str[0]
-				df3['p'] = df3['p'].str[0]
-				break
+	gp = [];a_l=len(gp1)
+	miss=0
+	for i in range(a_l):
+		if (gp0[i]+gp1[i]+gp2[i])>0:
+			gp.append(gp1[i]+(2*gp2[i]))
+		else:
+			gp.append(np.nan)
+			miss+=1
+	gp0n=np.nansum(gp0)
+	gp1n=np.nansum(gp1)
+	gp2n=np.nansum(gp2)
 
-			except RRuntimeError:
-				print("R had an issue with this one:", gl[1])				
-				df3 = pd.DataFrame({'beta':[-9],'se':[-9],'p':[-9],'conv':[0]})
-				break
+	#QC measures
+	info=info_calc(gp0,gp1,gp2,a_l,miss)
+	maf=maf_calc(gp0n,gp1n,gp2n,a_l,miss)
+	hwe=HWE_test(gp0n,gp1n,gp2n)
 
-		
-		
-		df1 = pd.DataFrame({'chr':[args.chr],'snp':[gl[1]],'bp':[gl[2]],'info':[info],'maf':[maf],'hwe':[hwe],'ea':[gl[3]],'nea':[gl[4]]})
+	#Analysis
+	sub['gz']=gp
+	sub[[args.t_pheno, args.et_pheno]] = sub[[args.t_pheno, args.et_pheno]].astype(int)
+	robjects.globalenv['sub'] = sub
+
+	while True:
+		try:
+			test=cmprsk.crr(ftime=sub[args.t_pheno],fstatus=sub[args.et_pheno],cov1=sub.iloc[:,2:],failcode=args.obs,cencode=0)
+			res=base.summary(test).rx2('coef')
+			#resconv=base.summary(test).rx2('conv') STILL NEED TO CODE CONV
+			df3 = pd.DataFrame({'beta':[res.rx('gz',1)],'se':[res.rx('gz',3)],'p':[res.rx('gz',5)],'conv':[1]})
+			df3['beta'] = df3['beta'].str[0]
+			df3['se'] = df3['se'].str[0]
+			df3['p'] = df3['p'].str[0]
+			break
+
+		except RRuntimeError:
+			print("R had an issue with this one:", gl[1])
+			df3 = pd.DataFrame({'beta':[-9],'se':[-9],'p':[-9],'conv':[0]})
+			break
+
+	df1 = pd.DataFrame({'chr':[args.chr],'snp':[gl[1]],'bp':[gl[2]],'info':[info],'maf':[maf],'hwe':[hwe],'ea':[gl[3]],'nea':[gl[4]]})
+	result=df1.join(df3)
+	return result;
+
+def QC(line, line_no ):
+	gl=line.split(' ')
+	gp0j=gl[5::3]
+	gp1j=gl[6::3]
+	gp2j=gl[7::3]
+	if args.chr:
+		gl[0]=args.chr
+	gl=gl[0:5]
+	gp0=[float(i) for i in gp0j]
+	gp1=[float(i) for i in gp1j]
+	gp2=[float(i) for i in gp2j]
+
+	gp = [];a_l=len(gp1)
+	miss=0
+	for i in range(a_l):
+		if (gp0[i]+gp1[i]+gp2[i])>0:
+			gp.append(gp1[i]+(2*gp2[i]))
+		else:
+			gp.append(np.nan)
+			miss+=1
+	gp0n=np.nansum(gp0)
+	gp1n=np.nansum(gp1)
+	gp2n=np.nansum(gp2)
+
+	#QC measures
+
+	info=info_calc(gp0,gp1,gp2,a_l,miss)
+	maf=maf_calc(gp0n,gp1n,gp2n,a_l,miss)
+	hwe=HWE_test(gp0n,gp1n,gp2n)
+	return(gp, info, maf, hwe, gl)
+
+def pycrgwas(gp, sub ):
+	#Analysis
+	subsnp=sub
+	subsnp['gz']=gp
+
+	if args.int:
+		subsnp['int'] = gp*sub[args.int]
+
+	subn = subsnp.dropna()
+	cph.fit(subn,duration_col=args.t_pheno,event_col='bin')
+	df3_1=cph.summary
+	return(df3_1)
+
+def output(df3_1, info, maf, hwe, gl ):
+	if args.verbose == 0:
+		df3_2 = df3_1.loc['gz',:]
+		df3 = pd.DataFrame({'beta':[df3_2.loc['coef']],'se':[df3_2.loc['se(coef)']],'p':[df3_2.loc['p']],'conv':[1]},dtype='float64') # potential to make this similar to verbose == 1 block in future... 
+		df1 = pd.DataFrame({'chr':[gl[0]],'test':[gl[1]],'snp':[gl[1]],'bp':[gl[2]],'info':[info],'maf':[maf],'hwe':[hwe],'ea':[gl[3]],'nea':[gl[4]]})
 		result=df1.join(df3)
-		print(result)
-		return result;
 
-def pycrgwas(line, line_no ):
-		gl=line.split(' ')	
-		gp0j=gl[5::3]
-		gp1j=gl[6::3] 
-		gp2j=gl[7::3]
-		
-		gp0=[float(i) for i in gp0j]
-		gp1=[float(i) for i in gp1j]
-		gp2=[float(i) for i in gp2j]
-		
-		gp = [];a_l=len(gp1)
-		miss=0
-		for i in range(a_l):
-			if (gp0[i]+gp1[i]+gp2[i])>0:
-				gp.append(gp1[i]+(2*gp2[i]))
-			else:
-				gp.append(np.nan)
-				miss+=1
-		gp0n=np.nansum(gp0)
-		gp1n=np.nansum(gp1)
-		gp2n=np.nansum(gp2)
-		
-		#QC measures
-		
-		info=info_calc(gp0,gp1,gp2,a_l,miss)
-		maf=maf_calc(gp0n,gp1n,gp2n,a_l,miss)
-		hwe=HWE_test(gp0n,gp1n,gp2n)
+	if args.verbose == 1:
+		df3_1.rename(index=str, columns={"coef":"beta","se(coef)":"se"}, inplace=True)
+		df3 = df3_1.loc[:,['beta','se','p']]
+		df1n = pd.DataFrame({'chr':[gl[0]],'snp':[gl[1]],'bp':[gl[2]],'info':[info],'maf':[maf],'hwe':[hwe],'ea':[gl[3]],'nea':[gl[4]],'conv':[1]})
+		df1 = pd.concat([df1n]*df3.shape[0])
+		df1.reset_index(drop=True, inplace=True)
+		df3.reset_index(drop=True, inplace=True)
+		result = pd.concat([df1,df3], axis=1)
+		result['test'] = df3_1.index
+		result['test'].replace(to_replace="gz",value=gl[1],inplace=True)
 
-		#Analysis
-		subsnp=sub
-		subsnp['gz']=gp
-		if args.int:
-			subsnp['int'] = gp*sub[args.int]
-
-		subn = subsnp.dropna()
-		cph.fit(subn,duration_col=args.t_pheno,event_col='bin')		
-		#cph.print_summary()
-		df3_1=cph.summary
-		
-		if args.verbose == 0:
-			df3_2 = df3_1.loc['gz',:]
-			df3 = pd.DataFrame({'beta':[df3_2.loc['coef']],'se':[df3_2.loc['se(coef)']],'p':[df3_2.loc['p']],'conv':[1]},dtype='float64') # potential to make this similar to verbose == 1 block in future... 
-			df1 = pd.DataFrame({'chr':[args.chr],'test':[gl[1]],'snp':[gl[1]],'bp':[gl[2]],'info':[info],'maf':[maf],'hwe':[hwe],'ea':[gl[3]],'nea':[gl[4]]})
-			result=df1.join(df3)
-		if args.verbose == 1:
-			df3_1.rename(index=str, columns={"coef":"beta","se(coef)":"se"}, inplace=True)
-			df3 = df3_1.loc[:,['beta','se','p']]
-			df1n = pd.DataFrame({'chr':[args.chr],'snp':[gl[1]],'bp':[gl[2]],'info':[info],'maf':[maf],'hwe':[hwe],'ea':[gl[3]],'nea':[gl[4]],'conv':[1]})
-			df1 = pd.concat([df1n]*df3.shape[0])
-			df1.reset_index(drop=True, inplace=True)
-			df3.reset_index(drop=True, inplace=True)
-			result = pd.concat([df1,df3], axis=1)
-			result['test'] = df3_1.index
-			result['test'].replace(to_replace="gz",value=gl[1],inplace=True)
-			
-		result['p'] = result['p'].apply(lambda x: round(x,5))
-		result[['info','maf','hwe']] = result[['info','maf','hwe']].apply(lambda x: round(x,2))
-		result[['beta','se']] = result[['beta','se']].apply(lambda x: round(x,4))
-		return result;
+	result['p'] = result['p'].apply(lambda x: round(x,5))
+	result[['info','maf','hwe']] = result[['info','maf','hwe']].apply(lambda x: round(x,2))
+	result[['beta','se']] = result[['beta','se']].apply(lambda x: round(x,4))
+	return result;
 
 
 
@@ -288,7 +299,7 @@ parser.add_argument('--gfile', type=str, default="",
 parser.add_argument('--sfile', type=str, default="",
 		    help='sample file needed')
 parser.add_argument('--ofile', type=str, default="",
-		    help='output file stem, e.g. <ofile>.out')					
+		    help='output file stem, e.g. <ofile>.out')
 parser.add_argument('--chr', type=str,
 		    help='chromosome number to use in output file')
 parser.add_argument('--obs', type=int, default=1,
@@ -307,6 +318,8 @@ parser.add_argument('--r', type=int, default=0,
 		    help='use 1 to perform analysis with R package cmprsk or 0 to perform analysis in Python package lifelines')
 parser.add_argument('--verbose', type=int, default=0,
 		    help='use 1 to print results of covariates tested at each SNP, includes interaction terms of specified')
+parser.add_argument('--thread', type=int, default=1,
+		    help='specify number of threads to use (please consider the hardware limitations of your computer/cluster)')
 args=parser.parse_args()
 
 if args.r == 1:
@@ -325,72 +338,76 @@ if args.r == 1:
 	#warnings.filterwarnings("ignore", category=RRuntimeError) # Sort out why this generates error!
 	robjects.r('sink("/dev/null")')
 
+if __name__ == "__main__":
+	print("##############################")
+	print("#      WELCOME TO CRAG       #")
+	print("##############################\n")
 
-print("##############################")
-print("#      WELCOME TO CRAG       #")
-print("##############################")
+	# Reduce sample to needed covs
 
-# Reduce sample to needed covs
+	sample = pd.read_csv(args.sfile,sep=" ") 
+	list=[args.t_pheno,args.et_pheno]
+	if args.covs!="":
+		list.extend(args.covs.split(','))
+	if args.int!="":
+		list.extend(args.int.split(','))
+	sub=sample[list]
+	sub=sub.drop([0])
+	sub[list] = sub[list].apply(pd.to_numeric)
+	sub['bin'] = np.where(sub[args.et_pheno] == args.obs, 1, 0)
 
-sample = pd.read_csv(args.sfile,sep=" ") 
-list=[args.t_pheno,args.et_pheno]
-if args.covs!="":
-	list.extend(args.covs.split(','))
-if args.int!="":
-	list.extend(args.int.split(','))
-sub=sample[list]
-sub=sub.drop([0])
-sub[list] = sub[list].apply(pd.to_numeric)
-sub['bin'] = np.where(sub[args.et_pheno] == args.obs, 1, 0)
+	if args.crtype==0:
 
-if args.crtype==0:
+		tmax=sub.loc[sub['bin']==1][args.t_pheno].max()
+		sub[args.t_pheno] = np.where((sub[args.et_pheno] != args.obs) & (sub[args.et_pheno] != 0), tmax, sub[args.t_pheno])
 
-	tmax=sub.loc[sub['bin']==1][args.t_pheno].max()
-	sub[args.t_pheno] = np.where((sub[args.et_pheno] != args.obs) & (sub[args.et_pheno] != 0), tmax, sub[args.t_pheno])
-	
-if args.crtype==2:
+	if args.crtype==2:
 
-	tmax=sub.loc[sub['bin']==1][args.t_pheno].max()
-	sub['bin'] = np.where(sub[args.et_pheno] > 0, 1, 0)
+		sub['bin'] = np.where(sub[args.et_pheno] > 0, 1, 0)
 
-sub=sub.drop(columns=[args.et_pheno])
-
+	sub=sub.drop(columns=[args.et_pheno])
 
 
-# Check for low variance
-print("#    CLINICAL ANALYSIS...    #")
-print("##############################")
 
-if args.covs:
-	low_var = pd.DataFrame(sub.var(0) < 10e-5)
-	print("\nResults of the low variance (<10e-5) test... (if True these are removed to avoid convergence issues)")
-	if args.int:
-		print(low_var.loc[args.covs.split(',') + args.int.split(',')])
-	if not args.int:
-		print(low_var.loc[args.covs.split(',')])
-		
-	lvf=low_var[low_var.iloc[:,0] == True]	
+	# Check for low variance
+	if args.covs:
 
-	if not lvf.empty:	
-		sub = sub.drop(lvf.index,axis = 1)
+		print("##############################")
+		print("#    CLINICAL ANALYSIS...    #")
+		print("##############################\n")
+		low_var = pd.DataFrame(sub.var(0) < 10e-5)
+		print("\nResults of the low variance (<10e-5) test... (if True these are removed to avoid convergence issues)")
+		if args.int:
 
-	print("\nResults of pre-GWAS multivariable analysis...")
-	cph.fit(sub,duration_col=args.t_pheno,event_col='bin')
-	cph.print_summary()
+			print(low_var.loc[args.covs.split(',') + args.int.split(',')])
 
-# Open gen file and perform calculation every line
-colNames = ('chr','snp','test','bp','ea','nea','p','info','maf','hwe','beta','se')
-#CHR SNP BP P INFO MAF HWE BETA SE CONV
-print("##############################")
-print("#   BEGIN GWAS ANALYSIS...   #")
-print("##############################")
-if __name__ == "__main__":	
-	num_workers = 1
-	
+		if not args.int:
+
+			print(low_var.loc[args.covs.split(',')])
+
+		lvf=low_var[low_var.iloc[:,0] == True]
+
+		if not lvf.empty:
+
+			sub = sub.drop(lvf.index,axis = 1)
+
+		print("\nResults of pre-GWAS multivariable analysis...")
+
+		cph.fit(sub,duration_col=args.t_pheno,event_col='bin')
+		cph.print_summary()
+
+	# Open gen file and perform calculation every line
+	colNames = ('chr','snp','test','bp','ea','nea','p','info','maf','hwe','beta','se')
+	#CHR SNP BP P INFO MAF HWE BETA SE CONV
+	print("##############################")
+	print("#   BEGIN GWAS ANALYSIS...   #")
+	print("##############################\n")
+	num_workers = args.thread
+
 	manager = Manager()
 	results = manager.list()
 	work = manager.Queue(num_workers)
-	
+
 	#start for workers
 	pool = []
 	for i in range(num_workers):
@@ -403,26 +420,22 @@ if __name__ == "__main__":
 		with gzip.open(args.gfile,'rt') as f:
 			iters = itertools.chain(f, (None,)*num_workers)
 			for num_and_line in enumerate(iters):
-				sys.stdout.write('\r')
-				sys.stdout.write("#     SNPS ANALYSED.." + str(i) + "    ##     TIME PER SNP.. {0:0.01f} s      #".format((time.time()-start)/i))
-				sys.stdout.flush()
-				work.put(num_and_line)
-	
+				gwasprogress()
+				i+=1
+
 	else:
 		with open(args.gfile,'rt') as f:
 			iters = itertools.chain(f, (None,)*num_workers)
 			for num_and_line in enumerate(iters):
-				sys.stdout.write('\r')
-				sys.stdout.write("#     SNPS ANALYSED.." + str(i) + "    ##     TIME PER SNP.. {0:0.01f} s      #".format((time.time()-start)/i))
-				sys.stdout.flush()
-				work.put(num_and_line)
+				gwasprogress()
 				i+=1
 	for p in pool:
 		p.join()
-		
+
 	f_results=pd.concat(results)
 	final=f_results.ix[:,colNames]
 	final.to_csv(args.ofile + ".out",sep=" ",index=False)
-print("##############################")
-print("#      END OF ANALYSIS       #")
-print("##############################")
+
+	print("\n\n##############################")
+	print("#      END OF ANALYSIS       #")
+	print("##############################\n")
