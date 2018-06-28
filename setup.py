69751d43fdc87ae38a0513f067906dd9a580b162
brfrancis <b.r.francis@outlook.com>
2018-06-28 15:45:37 +0100

    Another setup update

--- setup.py
+++ setup.py
@@ -10,17 +10,6 @@ setup(
     keywords="competing risks survival analysis statistics data analysis",
     url="https://github.com/brfrancis/CompetingRisksAnalysisGW",
     packages=['crag'],
-    long_description=readme_rst,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Topic :: Scientific/Engineering",
-        ],
     install_requires=[
       "subprocess",
       "sys",
